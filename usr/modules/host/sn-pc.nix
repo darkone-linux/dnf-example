3c0ec9f018a287228290d4f221ca64b15a5f649d
Darkone Linux <darkone@darkone.yt>
2025-01-30 20:36:48 -0300

    Refactorings & docs

--- usr/modules/host/sn-pc.nix
+++ usr/modules/host/sn-pc.nix
@@ -30,6 +30,33 @@ in
       #graphic.music.enable = true;
     };
 
+    # Hardware configuration is identical for all PCs
+    #imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
+    boot.initrd.availableKernelModules = [
+      "ata_piix"
+      "uhci_hcd"
+      "sr_mod"
+      "xen_blkfront"
+    ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/5b32fe4f-d1fe-44bb-907b-a5b8e4ed7621";
+      fsType = "ext4";
+    };
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/5719-4F30";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+    swapDevices = [ { device = "/dev/disk/by-uuid/9d3f5bf2-bba5-437d-adc4-ac22ebe3f026"; } ];
+    networking.useDHCP = lib.mkDefault true;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
     # Host specific state version
     system.stateVersion = "24.11";
   };
--- usr/modules/host/vbox.nix
+++ usr/modules/host/vbox.nix
@@ -21,6 +21,41 @@ in
       theme.advanced.enable = true;
     };
 
+    # Vbox common hardware configuration
+    boot.initrd.availableKernelModules = [
+      "ata_piix"
+      "ohci_pci"
+      "ehci_pci"
+      "ahci"
+      "sd_mod"
+      "sr_mod"
+    ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ ];
+    boot.extraModulePackages = [ ];
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/9b47d3f9-5cf1-417a-87d9-6e56d37ce27b";
+      fsType = "btrfs";
+      options = [ "subvol=@" ];
+    };
+    fileSystems."/home" = {
+      device = "/dev/disk/by-uuid/9b47d3f9-5cf1-417a-87d9-6e56d37ce27b";
+      fsType = "btrfs";
+      options = [ "subvol=@home" ];
+    };
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/1A9B-7196";
+      fsType = "vfat";
+      options = [
+        "fmask=0077"
+        "dmask=0077"
+      ];
+    };
+    swapDevices = [ { device = "/dev/disk/by-uuid/9f3bfa92-03e9-4e05-a013-3893b7b5c9c4"; } ];
+    networking.useDHCP = lib.mkDefault true;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    virtualisation.virtualbox.guest.enable = true;
+
     system.stateVersion = "25.05";
   };
 }
