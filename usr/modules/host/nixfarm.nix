26d9041b62eb5248282ab9e4290eb2b6703e95dd
Darkone Linux <darkone@darkone.yt>
2025-02-04 19:49:17 -0300

    NFS for SN homes

--- usr/modules/host/nixfarm.nix
+++ usr/modules/host/nixfarm.nix
@@ -15,10 +15,18 @@ in
     boot.loader.grub.enable = true;
     boot.loader.grub.device = "/dev/sda";
 
+    # Server for advanced user
     darkone = {
       host.server.enable = true;
       theme.advanced.enable = true;
     };
+
+    # NFS shares
+    services.nfs.server.enable = true;
+    services.nfs.server.exports = ''
+      /export/homes 192.168.11.0/24(rw,sync,insecure,no_subtree_check,fsid=homes)
+    '';
+    networking.firewall.allowedTCPPorts = [ 2049 ]; # NFSv4
 
     # AI
     services.ollama.enable = true;
